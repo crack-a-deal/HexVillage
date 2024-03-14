34c1fdf205b688711dc276c2361d5c7e943ea358
savkin.a <savkin.a@bk-s.pro>
2024-03-14 15:09:28 +0300

    Added axial distance methods

--- Assets/CodeBase/Distance.cs
+++ Assets/CodeBase/Distance.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public static class Distance
 {
@@ -6,7 +7,7 @@ public static class Distance
     {
         Vector2Int selectedCoords = CoordinateConversion.OffsetToAxial(selected);
         Vector2Int targetCoords = CoordinateConversion.OffsetToAxial(target);
-        return GetAxialDistance(selected, target);
+        return GetInlineAxialDistance(selected, target);
     }
 
     public static int GetAxialDistance(Vector2Int first, Vector2Int second)
@@ -16,14 +17,30 @@ public static class Distance
         return GetCubeDistance(fCubeCoord, sCubeCoord);
     }
 
+    public static int GetInlineAxialDistance(Vector2Int first,Vector2Int second)
+    {
+        return (Math.Abs(first.x - second.x) + Math.Abs(first.x + first.y - second.x - second.y) + Math.Abs(first.y - second.y)) / 2;
+    }
+
+    public static int GetAnotherAxielDistance(Vector2Int first, Vector2Int second)
+    {
+        var vec = Subtract(first, second);
+        return (Math.Abs(vec.x) + Math.Abs(vec.x + vec.y) + Math.Abs(vec.y)) / 2;
+    }
+
     public static int GetCubeDistance(Vector3Int f, Vector3Int s)
     {
-        Vector3Int vec = CubeSubtract(f, s);
+        Vector3Int vec = Subtract(f, s);
         return Mathf.Max(Mathf.Abs(vec.x), Mathf.Abs(vec.y), Mathf.Abs(vec.z));
     }
 
-    public static Vector3Int CubeSubtract(Vector3Int first, Vector3Int second)
+    public static Vector3Int Subtract(Vector3Int first, Vector3Int second)
     {
         return new Vector3Int(first.x - second.x, first.y - second.y, first.z - second.z);
+    }
+
+    public static Vector2Int Subtract(Vector2Int first, Vector2Int second)
+    {
+        return new Vector2Int(first.x - second.x, first.y - second.y);
     }
 }
