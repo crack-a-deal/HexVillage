bb1f67e15d087dd85608b81ec195c0285e6410a1
savkin.a <savkin.a@bk-s.pro>
2024-03-14 09:21:03 +0300

    improved positioning of hexagons in grid

--- Assets/CodeBase/HexGridLayout.cs
+++ Assets/CodeBase/HexGridLayout.cs
@@ -1,14 +1,22 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class HexGridLayout : MonoBehaviour
 {
-    private float width = 1, height = 1;
     [SerializeField] private int columnCount;
     [SerializeField] private int rowCount;
-    [SerializeField] private GameObject hexagonPrefab;
+    [SerializeField] private Hexagon hexagonPrefab;
 
-    private void Start()
+    [SerializeField] private bool isEvenColum = true;
+    [SerializeField] private Color evenColor;
+
+    [SerializeField] private Hexagon[,] hexagonGrid;
+
+    public Hexagon[,] Grid => hexagonGrid;
+
+    private void Awake()
     {
+        hexagonGrid = new Hexagon[columnCount, rowCount];
         LayoutGrid();
     }
 
@@ -18,26 +26,28 @@ public class HexGridLayout : MonoBehaviour
         {
             for (int q = 0; q < columnCount; q++)
             {
-                GameObject hexagon = Instantiate(hexagonPrefab, transform);
-                hexagon.transform.position = GetHexagonPositionFromCoordinates(new Vector2Int(q, r));
+                Hexagon hexagon = Instantiate(hexagonPrefab, transform);
+                hexagon.name = $"Hexagon [{q},{r}]";
+                hexagon.transform.position = GetHexagonPositionFromCoordinates(new Vector2Int(q, r)) + (Vector2)transform.position;
+
+                hexagon.Column = q;
+                hexagon.Row = r;
+                hexagonGrid[q, r] = hexagon;
             }
         }
     }
 
     private Vector2 GetHexagonPositionFromCoordinates(Vector2Int coordinates)
     {
-        int row = coordinates.y;
         int column = coordinates.x;
+        int row = coordinates.y;
 
         bool shoutOffset = column % 2 != 0;
-        float offset = shoutOffset ? width / 2 : 0;
+        float offset = shoutOffset ? 0.45f : 0f;
 
-        float horiz = 3 / 4 * width;
-        float vert = height;
-
-        float rowPosition = row * vert + offset;
-        float colPosition = column;
-        Debug.Log($"Hex coordinates Row:{row} Column:{column}\n rPosition:{rowPosition} cPosition:{colPosition}");
+        float rowPosition = row + offset - (0.1f * row);
+        float colPosition = column - (column * 0.2f);
+        rowPosition *= isEvenColum ? 1 : -1;
         return new Vector2(colPosition, rowPosition);
     }
 }
