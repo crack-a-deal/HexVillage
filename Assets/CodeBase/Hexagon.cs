752cd3ee3bb1717a79b0f45f2c1e9ad71123a368
savkin.a <savkin.a@bk-s.pro>
2024-03-14 09:34:43 +0300

    Added hexagon selection

--- Assets/CodeBase/Hexagon.cs
+++ Assets/CodeBase/Hexagon.cs
@@ -2,12 +2,28 @@ using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Hexagon : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class Hexagon : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
+    public event Action<Hexagon> Selected;
+
     [SerializeField] private GameObject outline;
+    [SerializeField] private SpriteRenderer hexagonRenderer;
+    [SerializeField] private Color selectedColor;
 
     public int Column { get; set; }
     public int Row { get; set; }
+
+    private Color _baseColor;
+
+    private void Awake()
+    {
+        _baseColor = hexagonRenderer.color;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        SelectHexagon();
+    }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
@@ -22,5 +38,19 @@ public class Hexagon : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     private void ActiveOutline(bool isActive)
     {
         outline.SetActive(isActive);
+    }
+
+    public void SetSelectionColor()
+    {
+        if (hexagonRenderer.color == _baseColor)
+            hexagonRenderer.color = selectedColor;
+        else
+            hexagonRenderer.color = _baseColor;
+    }
+
+    private void SelectHexagon()
+    {
+        SetSelectionColor();
+        Selected?.Invoke(this);
     }
 }
